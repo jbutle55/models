e7c7aced94059d6ac5c9b6efadea6cc8c38fa95c
jbutle55 <justin.butler12@hotmail.ca>
2021-04-14 14:35:51 -0600

    fix config filename

--- Run_SSD.py
+++ Run_SSD.py
@@ -75,7 +75,7 @@ def main(args):
     ##################################
     video_path = args.video
     num_classes = 1
-    pipeline_config = 'research/object_detection/configs/tf2/ssd_resnet50_v1_fpn_640x640_coco17_tpu-8.config'
+    pipeline_config = 'research/object_detection/configs/tf2/ssd_resnet101_v1_fpn_1024x1024_coco17_tpu-8.config'
     checkpoint_path = 'research/object_detection/test_data/checkpoint/ckpt-0'
 
     # MODEL CREATION
