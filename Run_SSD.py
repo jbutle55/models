0e414c7c93a4ca729c7e56322746addc7120c457
jbutle55 <justin.butler12@hotmail.ca>
2021-04-16 15:27:53 -0600

    Change min thresh to 0.75

--- Run_SSD.py
+++ Run_SSD.py
@@ -225,7 +225,7 @@ def main(args):
                     category_index,
                     use_normalized_coordinates=True,
                     max_boxes_to_draw=None,
-                    min_score_thresh=0.0,
+                    min_score_thresh=0.75,
                     agnostic_mode=True,
                     keypoints=None,
                     keypoint_scores=None,
